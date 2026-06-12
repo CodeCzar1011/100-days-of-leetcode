d47759369b1fd726ad5207c596f5a9e8263b788f
Lavanya Gehlot <lavanya12342121@gmail.com>
2026-06-12 23:14:30 +0530

    Add files via upload

---
+++ Day-009/Sort_Colors.py
@@ -0,0 +1,19 @@
+#75
+class Solution:
+    def sortColors(self, nums):
+        start = 0
+        mid = 0
+        end = len(nums) - 1
+
+        while mid <= end:
+            if nums[mid] == 0:
+                nums[start], nums[mid] = nums[mid], nums[start]
+                start += 1
+                mid += 1
+
+            elif nums[mid] == 1:
+                mid += 1
+
+            else:  # nums[mid] == 2
+                nums[mid], nums[end] = nums[end], nums[mid]
+                end -= 1
