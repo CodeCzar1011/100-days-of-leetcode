b857d1e3695cea67bb8ce3fd447dc4b0dd2e3918
Lavanya Gehlot <lavanya12342121@gmail.com>
2026-06-20 21:18:34 +0530

    Add files via upload

---
+++ Day-017/Valid_Palindrome.py
@@ -0,0 +1,16 @@
+#125
+class Solution(object):
+    def isPalindrome(self, s):
+        # clean string
+        s = ''.join(c.lower() for c in s if c.isalnum())
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
